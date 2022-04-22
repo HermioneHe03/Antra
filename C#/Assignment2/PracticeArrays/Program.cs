0c7a6c1798a2bba3aa001f9721d1b87779c4af10
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-04-21 21:16:09 -0400

    Add files via upload

---
+++ C#/Assignment2/PracticeArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeArrays;
+//1.String: is immutable. I think it could store some important data.
+//  StringBuilder: is mutable. It could be used in methods so that we can change the text based on needs.
+//2. Base class for arrays: Object class.
+//3. Sort an array: Array.Sort()
+//4. Get the total number of elements in an array: Array.Length
+//5. Store multiple data types in System.Array: object array could store multiple data types in an array.
+//6. Difference between the System.Array.CopyTo() and System.Array.Clone():
+//   Clone(): returns a new array (a shallow copy) object containing all the elements in the original array. 
+//   CopyTo(): copies the elements into another existing array. Both perform a shallow copy.
+
+Class1 demo = new Class1();
+//demo.CopyAnArray( new []{ 1,2,3});
+//demo.CheckPrime(17);
+//demo.FindPrimesInRange(1, 100);
+//demo.SumAfterRotation(new[] { 3, 2, 4, -1 }, 2);
+//demo.MostFrequentNumber(new[] {7, 8, 9, 10, 11, 12, 13, 7,7,7});
+//demo.ParseUrl("https://www.apple.com/iphone");
