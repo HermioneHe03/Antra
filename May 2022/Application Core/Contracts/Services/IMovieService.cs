34fd24f723d8e9b8c2c49f1f42d7353bb6402c5f
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-05-11 17:22:14 -0400

    Add files via upload

---
+++ May 2022/Application Core/Contracts/Services/IMovieService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application_Core.Contracts.Repositories;
+using MovieShopMVC.Models;
+
+namespace Application_Core.Contracts.Services
+{
+    public interface IMovieService
+    {
+       //home/index action method
+        List<MovieCardModel> GetTop30GrossingMovies();
+    }
+}
---
+++ May 2022/Application Core/Models/MovieCardModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieShopMVC.Models
+{
+    public class MovieCardModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PosterUrl { get; set; }
+    }
+}
