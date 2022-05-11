94f2d4cde7b4834d470fa7c6844029cd03af127a
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-05-11 17:24:54 -0400

    Add files via upload

---
+++ May 2022/MovieShop/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using MovieShop.Models;
+using Infrastructure.Repositories;
+
+namespace MovieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Action methods
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var movieService = new MovieService();
+            var movieCards = movieService.GetTop30GrossingMovies();
+            return View(movieCards);
+        }
+
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopRatedMovies()
+        {
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ May 2022/MovieShop/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            // go to movies talbe and get the movie details by ID
+            // connect to SQL server and execute the SQL query
+            //select * from Movie where id=2
+            // get the movies entity (object)
+            // Repositories => Data access Logic
+            //Services => Business Logic
+            // Controllers action methods => Services methods => Repository methods => SQL database
+            // get the mode data from the sevices and send the data to the views(M)
+            // Onion architecture or N-Layer architecture
+            return View();
+        }
+    }
+}
