a1e505ad80b98f7d29e29ab51a8163284164c169
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-05-12 17:09:17 -0400

    Add files via upload

--- May 2022/Infrastructure/Services/MovieTestService.cs
+++ May 2022/Infrastructure/Services/MovieTestService.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Application_Core.Contracts.Repositories;
 using Application_Core.Contracts.Services;
 using Infrastructure.Repositories;
 using Application_Core.Models;
 
 namespace Infrastructure.Services
 {
-    public class MovieService: IMovieService
+    public class MovieTestService: IMovieService
     {
-        private readonly IMovieRepository _movieRepository;
-        public MovieService(IMovieRepository movieRepository)
-        {
-            _movieRepository = movieRepository;
-        }
         public List<MovieCardModel> GetTop30GrossingMovies()
         {
             //call the movierepository
             //get the entity class data and map them in to model class data
-            // var movieRepo = new MovieRepository();
-            //var movies = movieRepo.GetTop30GrossingMovies();
-            var movies = _movieRepository.GetTop30GrossingMovies();
-
+            var movieRepo = new MovieRepository();
+            var movies = movieRepo.GetTop30GrossingMovies().Take(6);
             var movieCards = new List<MovieCardModel>();
             foreach (var movie in movies)
             {
