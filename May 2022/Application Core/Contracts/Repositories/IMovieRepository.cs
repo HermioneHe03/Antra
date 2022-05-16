f7867d53440b777ef52cfa7a17c5caa65e05d582
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-05-16 10:01:55 -0400

    Add files via upload

--- May 2022/Application Core/Contracts/Repositories/IMovieRepository.cs
+++ May 2022/Application Core/Contracts/Repositories/IMovieRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Application_Core.Entities;
+using ApplicationCore.Entities;
 
 namespace Application_Core.Contracts.Repositories
 {
